869a96322f7506a1af2db78d530d9fbdad93edec
evanpkj <evanpkj@wemakeprice.com>
2014-01-16 11:28:21 +0900

    eclipse commit

--- test/src/com/example/test/MainActivity.java
+++ test/src/com/example/test/MainActivity.java
@@ -11,6 +11,8 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 	}
+	// fjdsnalkfjsdalkfjsdlakfj
+	
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
